5659519a3c0d4464b31f6aeb1516f9900e0029c8
Nursel <nhazar2147@gmail.com>
2025-05-18 21:11:33 +0200

    Update main.js

--- main.js
+++ main.js
@@ -1,6 +1,8 @@
 const navbar = document.querySelector(".navbar");
 const menuBtn = document.querySelector("#menu-btn");
 
+
+
 menuBtn.addEventListener("click", () => {
     
     navbar.classList.toggle("active");
